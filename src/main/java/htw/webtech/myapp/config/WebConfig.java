95e6c634d53f2c6dd39d7d2206a992a50c3b36d5
Arthur <arthurlenschmidt@gmail.com>
2025-11-07 11:27:58 +0100

    WebConfig hinzugefügt - "CORS error"

---
+++ src/main/java/htw/webtech/myapp/config/WebConfig.java
@@ -0,0 +1,15 @@
+package htw.webtech.myapp.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // Oder "/**" für alle Pfade
+                .allowedOrigins("http://localhost:5173") // Die URL deines Frontends
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+    }
+}
